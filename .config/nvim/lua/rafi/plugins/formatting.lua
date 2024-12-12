1d09351351a7ebf7cf8551eef9062fa5ccdc16c3
daiyabarus <daiyabarus@gmail.com>
2024-12-12 12:14:27 +0700

    upd: LazyExtras config/lang

--- .config/nvim/lua/rafi/plugins/formatting.lua
+++ .config/nvim/lua/rafi/plugins/formatting.lua
@@ -9,9 +9,17 @@ return {
 	-- Lightweight yet powerful formatter plugin
 	{
 		'stevearc/conform.nvim',
-		-- stylua: ignore
 		keys = {
 			{ '<leader>cic', '<cmd>ConformInfo<CR>', silent = true, desc = 'Conform Info' },
 		},
+			formatters_by_ft = {
+		lua = { 'stylua' },
+		-- Conform can also run multip
+		python = {'black', 'isor'},
+		--
+		-- You can use a sub-list to tell conform to run *until* a formatter
+		-- is found.
+		javascript = { { "prettierd", "prettier" } },
 	},
+},
 }
